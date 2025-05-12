ede78fcad5957f7eb70ceef64d91dfe310236db8
Elias Rojas <eliasr@CARLOSs-MacBook-Pro.local>
2025-05-12 17:33:23 -0600

    first commit

---
+++ src/main/java/com/eliasr/CyclingAPI/CyclingApiApplication.java
@@ -0,0 +1,13 @@
+package com.eliasr.CyclingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliasr/CyclingAPI/CyclingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliasr.CyclingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
